92da488903070ba704f339ce5a6f04c85c97bd25
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-08-04 23:43:02 +0300

    Make filter by favorite

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -4,13 +4,17 @@ import { ctrlWrapper } from "../decorators/index.js";
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, ...query } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email ");
+  const result = await Contact.find(
+    { owner, ...query },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "name email ");
   res.json(result);
 };
 
